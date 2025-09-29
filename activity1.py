3eec755e3ff8e65c13864b18f5b13c4bd087dde9
roytheboyyy <roy.mou.safa@gmail.com>
2025-09-29 19:32:28 +0300

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,11 @@
+tuple1 = ("tupple",True,3.2,1)
+print(tuple1)
+
+tuple1 = (4, 6, 2, 8, 3, 1)
+print(tuple1)
+tuple1 = tuple1 + (9,)
+print(tuple1)
+tuple1 = (50, 10, 60, 70 ,50)
+print(tuple1.count(50))
+tuple1 = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+_slice = tuple1[3:5]
